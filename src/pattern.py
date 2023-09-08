e2400cbbf7bccf1718162f4a43712c97f4101212
EllenVe <143783633+EllenVe@users.noreply.github.com>
2023-09-08 09:46:30 +0200

    test

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,13 @@
 
+a = 6
+for i in range(1, a ):
+    b = ('* ' * i)
+    b = b[:-1]
+    print(b)
+for i in range(a-2, 0 , -1):
+    c = ('* ' * i)
+    c = c[:-1]
+    print(c)
+
+    
 # Print the pattern
--- src/print-1-10.py
+++ src/print-1-10.py
@@ -1,2 +1,8 @@
 
+a = 1
+
+while a < 11: 
+    print(a)
+    a += 1     
+
 # Print the numbers described in the exercise
